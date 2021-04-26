702e069cfe5046a63a591095142c42ae34429918
gchamort <guillaume.p.chanut@gmail.com>
2021-04-26 11:23:01 +0200

    mega clean de code de dingue

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
- <head>
-  <title>Test CI/CD</title>
- </head>
- <body>
- <?php
+    <head>
+        <title>Test CI/CD</title>
+    </head>
+<body>
+<?php
     echo '<h1>Bonjour, monde!</h1><br>';
     echo $_SERVER['REMOTE_ADDR'];
 ?>
- </body>
+</body>
 </html>
