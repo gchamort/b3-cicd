352e109fd77c47a8ac6d8f152eb935b9be782905
gchamort <guillaume.p.chanut@gmail.com>
2021-04-26 11:17:31 +0200

    ouais

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>Test CI/CD</title>
  </head>
  <body>
- <?php echo '<h1>Bonjour, monde!</h1>'; ?>
+ <?php
+    echo '<h1>Bonjour, monde!</h1><br>';
+    echo $_SERVER['REMOTE_ADDR'];
+?>
  </body>
 </html>
